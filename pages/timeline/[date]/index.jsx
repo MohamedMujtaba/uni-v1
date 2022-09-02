2704e33f57f9bba1cda7f6dc245ceea9d39bbeaa
M J <mohamedmujtaba0210@gmail.com>
2022-09-02 17:54:52 +0200

    Go back to clinte side render

--- pages/timeline/[date]/index.jsx
+++ pages/timeline/[date]/index.jsx
@@ -6,11 +6,12 @@ import Lecture from "../../../components/Lecture";
 import axios from "axios";
 import { useToast } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-const Day = ({ lectures }) => {
+const Day = () => {
   const [params, setParams] = useState({ year: "", dep: "" });
   const router = useRouter();
-  // const [lectures, SetLectures] = useState([]);
+  const [lectures, SetLectures] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const toast = useToast();
   useEffect(() => {
     let x = document.cookie;
@@ -36,92 +37,73 @@ const Day = ({ lectures }) => {
     ];
     return weekday[d?.getDay()];
   };
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       setLoading(true);
-  //       // `https://uni-api-v1.vercel.app/api/v1/lecture?date=${router.query["date"]}&dep=${params.dep}&year=${params.year}`
-  //       const res = await axios.get(
-  //         `https://uni-api-v1.vercel.app/api/v1/lecture?date=${router.query["date"]}&dep=${params.dep}&year=${params.year}`
-  //       );
-  //       SetLectures(res.data.lectures);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setLoading(false);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        // `https://uni-api-v1.vercel.app/api/v1/lecture?date=${router.query["date"]}&dep=${params.dep}&year=${params.year}`
+        const res = await axios.get(
+          `https://uni-api-v1.vercel.app/api/v1/lecture?date=${router.query["date"]}&dep=${params.dep}&year=${params.year}`
+        );
+        SetLectures(res.data.lectures);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
 
-  //       console.log(error);
+        console.log(error);
 
-  //       toast({
-  //         title: "Something went wrong",
-  //         description: "Please reload the page",
-  //         status: "error",
-  //         duration: 5000,
-  //         isClosable: true,
-  //       });
-  //     }
-  //   };
-  //   getData();
-  // }, [params.dep, params.year, router.query, toast]);
-  // if (loading) {
-  //   return (
-  //     <Flex w="100%" h="100%" alignItems="center" justifyContent="center">
-  //       <Spinner size="lg" />
-  //     </Flex>
-  //   );
-  // } else {
-  if (lectures?.length === 0) {
-    return (
-      <Flex h="30vh" w="100%" alignItems="center" justifyContent="center">
-        <Heading size="lg">No Lectures for now</Heading>
-      </Flex>
-    );
-  }
-
-  return (
-    <Container>
-      <Flex flexDirection="column" w="100%">
-        <Flex
-          w="100%"
-          maxW="50vw"
-          alignItems="center"
-          justifyContent="space-between"
-          marginBottom="4"
-        >
-          <IconButton
-            variant="ghost"
-            icon={<ArrowBackIcon />}
-            onClick={() => router.back()}
-          />
-          <Heading size="sm">{router.query["date"]}</Heading>
-          {/* <Heading size="sm">{getDayOfTheWeek(router.query["date"])}</Heading> */}
-        </Flex>
-        <Flex w="100%" gap="1rem" wrap="wrap">
-          {lectures.map((i) => {
-            return <Lecture i={i} key={i._id} />;
-          })}
-        </Flex>
-      </Flex>
-    </Container>
-  );
-};
-// };
-
-export const getServerSideProps = async (context) => {
-  const { req, res, params } = context;
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=60, stale-while-revalidate=59"
-  );
-  try {
-    const data = await axios.get(
-      `https://uni-api-v1.herokuapp.com/api/v1/lecture?date=${params.date}&dep=${req.cookies["major"]}&year=${req.cookies["year"]}`
-    );
-    // console.log(data);
-    return {
-      props: { lectures: data.data.lectures },
+        toast({
+          title: "Something went wrong",
+          description: "Please reload the page",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     };
-  } catch (error) {
-    console.log(error);
+    getData();
+  }, [params.dep, params.year, router.query, toast]);
+  if (loading) {
+    return (
+      <Flex w="100%" h="100%" alignItems="center" justifyContent="center">
+        <Spinner size="lg" />
+      </Flex>
+    );
+  } else {
+    if (lectures.length === 0) {
+      return (
+        <Flex h="30vh" w="100%" alignItems="center" justifyContent="center">
+          <Heading size="lg">No Lectures for now</Heading>
+        </Flex>
+      );
+    }
+
+    return (
+      <Container>
+        <Flex flexDirection="column" w="100%">
+          <Flex
+            w="100%"
+            maxW="50vw"
+            alignItems="center"
+            justifyContent="space-between"
+            marginBottom="4"
+          >
+            <IconButton
+              variant="ghost"
+              icon={<ArrowBackIcon />}
+              onClick={() => router.back()}
+            />
+            <Heading size="sm">{router.query["date"]}</Heading>
+            {/* <Heading size="sm">{getDayOfTheWeek(router.query["date"])}</Heading> */}
+          </Flex>
+          <Flex w="100%" gap="1rem" wrap="wrap">
+            {lectures.map((i) => {
+              return <Lecture i={i} key={i._id} />;
+            })}
+          </Flex>
+        </Flex>
+      </Container>
+    );
   }
 };
 
